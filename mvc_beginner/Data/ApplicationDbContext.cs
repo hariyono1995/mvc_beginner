a8d963b238e0224af19d6123e199a4b020da6c18
hariyono <96717662+hariyono1995@users.noreply.github.com>
2022-08-14 20:57:40 +0700

    Add project files.

---
+++ mvc_beginner/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvc_beginner.Models;
+
+namespace mvc_beginner.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
